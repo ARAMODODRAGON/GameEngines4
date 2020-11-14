f00097053261b90b8882f23c16519c9c8eeeb6f1
Domara Shlimon <domara2000@gmail.com>
2020-11-14 01:21:44 -0500

    json stuff

--- GameEngines4/Main.cpp
+++ GameEngines4/Main.cpp
@@ -1,19 +1,34 @@
 #include "Engine/Core/CoreEngine.h"
 #include "Game/Game1.h"
+#include <nlohmann\json.hpp>
+#include <fstream>
+using nlohmann::json;
 
 int main(int argc, char* argv[]) {
-	
+
+	json j = nullptr;
+	std::fstream stream("Config.json");
+	if (stream.is_open()) {
+		stream >> j;
+		stream.close();
+	}
+
 	// set game
 	CoreEngine::GetSingleton()->SetIGame(new Game1(), RendererType::OpenGL);
 
 	// create engine
-	if (!CoreEngine::GetSingleton()->OnCreate("title", 1280, 720)) {
+	std::string title = j["title"];
+	uvec2 size;
+	size.x = j["size"]["width"];
+	size.y = j["size"]["height"];
+
+	if (!CoreEngine::GetSingleton()->OnCreate(title, size.x, size.y)) {
 		return 1;
 	}
 
 	// start main loop
 	CoreEngine::GetSingleton()->Run();
-	
+
 	CoreEngine::GetSingleton()->OnDestroy();
 
 	return 0;
